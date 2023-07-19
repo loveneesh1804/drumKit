fc47a267385b27c4ea4ec1a9bf794d620b69a5bf
loveneesh1804 <139100507+loveneesh1804@users.noreply.github.com>
2023-07-19 11:34:20 +0530

    modify

--- script.js
+++ script.js
@@ -18,38 +18,38 @@ function clickHandle(key){
         
         switch (key){
                 case "w":
-                        var audio = new Audio("/crash.mp3");
+                        var audio = new Audio("./crash.mp3");
                         audio.play();
                 
                 break;
                 
                 case "a":
-                        var audio1 = new Audio("/kick-bass.mp3");
+                        var audio1 = new Audio("./kick-bass.mp3");
                         audio1.play();
                 break;
 
                 case "s":
-                        var audio2 = new Audio("/snare.mp3");
+                        var audio2 = new Audio("./snare.mp3");
                         audio2.play();
                 break;
 
                 case "d":
-                        var audio3 = new Audio("/tom-1.mp3");
+                        var audio3 = new Audio("./tom-1.mp3");
                         audio3.play();
                 break;
 
                 case "j":
-                        var audio4 = new Audio("/tom-2.mp3");
+                        var audio4 = new Audio("./tom-2.mp3");
                         audio4.play();
                 break;
 
                 case "k":
-                        var audio5 = new Audio("/tom-3.mp3");
+                        var audio5 = new Audio("./tom-3.mp3");
                         audio5.play();
                 break;
 
                 case "l":
-                        var audio6 = new Audio("/tom-4.mp3");
+                        var audio6 = new Audio("./tom-4.mp3");
                         audio6.play();
                 break;
 
